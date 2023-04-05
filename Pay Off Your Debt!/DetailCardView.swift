178fbd221474c0977f1bf0fc6671ce23cc282030
Ziterz <ziady.mubaraq@gmail.com>
2023-04-05 23:07:49 +0700

    tadi tambahin screen baru

---
+++ Pay Off Your Debt!/DetailCardView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailCardView.swift
+//  Pay Off Your Debt!
+//
+//  Created by Ziady Mubaraq on 05/04/23.
+//
+
+import SwiftUI
+
+struct DetailCardView: View {
+    var body: some View {
+        Text("Hello, Roli!")
+    }
+}
+
+struct DetailCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailCardView()
+    }
+}
