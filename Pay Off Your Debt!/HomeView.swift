d702c7a55ac128639bd9653eddc5b92f6d3254c8
marufboy <afif23ixa@gmail.com>
2023-04-11 09:12:16 +0700

    setup core data

--- Pay Off Your Debt!/HomeView.swift
+++ Pay Off Your Debt!/HomeView.swift
@@ -7,12 +7,96 @@
 
 import SwiftUI
 
+struct DebtType: Identifiable {
+    let id = UUID()
+    var name: String
+}
+
+//struct Person: Identifiable {
+//    let id = UUID()
+//    var name: String
+//    var phoneNumber: String
+//}
+//
+//struct Wallet: Identifiable {
+//    let id = UUID()
+//    var person: Person
+//    var totalAmount: Int
+//    var debt: [Debt]
+//    var repay: [Repay]
+//
+//    func addDebt(debt: Debt) {
+//
+//    }
+//}
+//
+//
+//struct Debt: Identifiable {
+//    let id = UUID()
+//    var person: Person
+//    var amount: String
+//    var personalNote: String
+//    var repaymentDate: Date
+//    var type: DebtType
+//
+//    init(person: Person, amount: String, personalNote: String, repaymentDate: Date, type: DebtType) {
+//        self.person = person
+//        self.amount = amount
+//        self.personalNote = personalNote == "" ? "-" : personalNote
+//        self.repaymentDate = repaymentDate
+//        self.type = type
+//    }
+//}
+//
+//struct Repay: Identifiable {
+//    let id = UUID()
+//    let idWallet: Int
+//    var date: Date
+//    var amount: Int
+//}
+
 struct HomeView: View {
+    @State var goToPage: Bool = false
+    
+    // Initial Data
+    @State var People: [Person] = []
+    @State var debtTypes: [DebtType] = [
+        DebtType(name: "Lent"),
+        DebtType(name: "Owe")]
+//    @State var debts: [Debt] = [Debt]()
+//    @State var repays: [Repay] = []
+//    @State var wallets: [Wallet] = []
+    
+    
+    // Status
+    @State private var showingSheet: Bool = false
+    @State private var showingAlert: Bool = false
+    @State private var showingContacts: Bool = false
+    
+    // Data
+    @State private var person: String = "Contact"
+    @State private var amount: String = "0"
+    @State private var personalNote: String = ""
+    @State private var repaymentDate: Date = Date()
+    @State private var debtType: String = "Owe"
+    
+    
     @State private var isSummaryShow : Bool = true
     @State private var summary : Int = 10000
     
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [],
+        animation: .default)
+    private var wallets: FetchedResults<Wallet>
+    
     var body: some View {
         NavigationStack{
+            
+            NavigationLink(destination: DetailView(), isActive: $goToPage) {
+                EmptyView()
+            }
             VStack {
                 //Header Debt
                 VStack{
@@ -57,12 +141,15 @@ struct HomeView: View {
                     // TODO: Create debtcard view with a list
                     ScrollView{
                         VStack(spacing: 20){
-                            DebtCard(cardColor: "CardPrimary")
-                            DebtCard(cardColor: "CardPrimary")
-                            DebtCard(cardColor: "CardSecondary")
-                            DebtCard(cardColor: "CardPrimary")
-                            DebtCard(cardColor: "CardPrimary")
-                            DebtCard(cardColor: "CardSecondary")
+                            ForEach(wallets, id: \.id) {
+                                wallet in DebtCard(personName: wallet.person?.name ?? "Unknown", type: "owe", totalAmount: wallet.totalAmount, cardColor: "CardPrimary")
+                            }
+//                            DebtCard(cardColor: "CardPrimary")
+//                            DebtCard(cardColor: "CardPrimary")
+//                            DebtCard(cardColor: "CardSecondary")
+//                            DebtCard(cardColor: "CardPrimary")
+//                            DebtCard(cardColor: "CardPrimary")
+//                            DebtCard(cardColor: "CardSecondary")
                         }
                     }
                     .padding(.bottom)
@@ -71,11 +158,90 @@ struct HomeView: View {
                 }
                 .padding(.horizontal, 20)
                 .background(.white)
-                .cornerRadius(36)
+                .cornerRadius(24)
                 .padding(.bottom, -50)
                 //.cornerRadius(36, corners: [.topLeft, .topRight])
             }
             .background(.blue)
+            .toolbar{
+                Button{
+                    showingSheet.toggle()
+                }label: {
+                    Image(systemName: "plus")
+                }
+                
+            }
+            .sheet(isPresented: $showingSheet) {
+                    VStack {
+                        List {
+                            Section {
+                                HStack {
+                                    Text(person)
+                                    Spacer()
+                                    Label("Contact", systemImage: "plus.circle")
+                                        .font(.title3)
+                                        .labelStyle(.iconOnly)
+                                }
+                                .onTapGesture {
+                                    self.showingContacts = true
+                                }
+                                .sheet(isPresented: $showingContacts) {
+                                    ContactView(showingContact: $showingContacts, contact: $person)
+                                }
+                                TextField("IDR0.00", text: $amount)
+                                HStack {
+                                    Text("Repayment Date")
+                                    Spacer()
+                                    DatePicker(
+                                        "",
+                                        selection: $repaymentDate,
+                                        in: Date()...,
+                                        displayedComponents: [.date]
+                                    )
+                                }
+                                TextField("Personal Note", text: $personalNote)
+                            }
+                            Section {
+                                HStack {
+                                    Text("Type")
+                                    Spacer()
+                                    Picker("", selection: $debtType) {
+                                        Text("Owe").tag("Owe")
+                                        Text("Lent").tag("Lent")
+                                    }
+                                }
+                            }
+                            Section {
+                                Button {
+                                    self.showingSheet.toggle()
+                                    addDebt() // TODO:
+                                    self.goToPage = true
+                                } label: {
+                                    Text("Save")
+                                        .frame(maxWidth: .infinity)
+                                }
+                                .listRowBackground(Color.accentColor)
+                                .foregroundColor(Color.white)
+                            }
+                            
+                        }
+                        .scrollDisabled(true)
+                        .scrollContentBackground(.hidden)
+                        .alert(isPresented: self.$showingAlert) {
+                            Alert(title: Text("Contact on Pressed!"), dismissButton: .default(Text("Dismiss")))
+                        }
+                        .navigationBarTitle("New Debt",displayMode: .inline)
+                        .navigationBarItems(
+                            leading:Button("Cancel", action: {
+                                self.showingSheet.toggle()
+                            })
+                        )
+                    }
+                    .background(Color(UIColor.systemGroupedBackground))
+                    .presentationDetents([.large])
+                    .presentationDragIndicator(.visible)
+                
+            }
         }
     }
     
@@ -101,18 +267,64 @@ struct HomeView: View {
     func stringToAsterisk(value : String) -> String{
         return String(repeating: "*", count: value.count)
     }
+    
+    
+    private func addDebt() {
+        withAnimation {
+            
+            // TODO:  Kalo belum ada walletnya create baru. Kalo dah ada langsung tambahin debt di walletnya
+            // TODO: kalo target person blm ada create baru, kalo dah ada langsung tambahin
+            let newWallet = Wallet(context: viewContext)
+            let newDebt = Debt(context: viewContext)
+            let newPerson = Person(context: viewContext)
+            
+            newWallet.id = UUID()
+            newWallet.totalAmount = 10000 // TODO: integrate
+            
+            newPerson.id = UUID() //
+            newPerson.name = "ME" // TODO:
+            
+            newDebt.id = UUID()
+            newDebt.amount = 20000 // TODO: Integrate
+            newDebt.personalNote = "note" // TODO: Integrate
+            newDebt.type = "owe" // TODO: Integrate
+            newDebt.repaymentDate = Date()
+            
+            newWallet.person = newPerson
+            newWallet.debt = newDebt
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    private func AddRepay() {
+        // TODO:
+    }
 }
 
 //Debt Card Components
 struct DebtCard: View {
+    @State var personName: String
+    @State var type: String // TODO: hitung banyakan hutang atau ngutangin
+    @State var totalAmount: Int32
+//    @State var createdAt: String // TODO:
+    
     var cardColor : String
+    
     var body: some View {
         NavigationLink(destination: EmptyView()){
             HStack(spacing: 60.0){
-                Text("Alexander MonMon")
+                Text("\(personName)")
                 VStack(){
-                    Text("I Owe RP. 25.000")
-                    Text("31 days ago")
+                    Text("I \(type) RP. \(totalAmount)") // TODO: Format currency
+                    Text("31 days ago") // TODO
                 }
                 
             }
---
+++ Pay Off Your Debt!/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  Pay Off Your Debt!
+//
+//  Created by Muhammad Afif Maruf on 11/04/23.
+//
+
+import Foundation
